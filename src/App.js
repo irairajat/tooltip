2de67cec61b38cdfc822dcae9c4fdbad0b679d84
Rajat Rai <irairajat@gmail.com>
2024-04-01 02:50:31 +0530

    Comment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 import './Tooltip.js';
 import Tooltip from './Tooltip.js';
 
+// Main Function
 function App() {
   return (
     <div>
       <div class="tooltip-box">
+        {/* Tooltip component with default position is top */}
       <Tooltip position="top" />
       </div>
     </div>
